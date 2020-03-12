e54d54f949f209c434b34ade6d5a0d86191805de
Lavell Wesley <lavellthedev+github@gmail.com>
2020-03-12 09:44:19 -0400

    finished 2 out of 10 problems

---
+++ solutions.js
@@ -0,0 +1,29 @@
+//ARRAY METHODS PRACTICE PROBLEMS
+
+//#1 Translate border-left-width to borderLeftWidth
+
+// const camelize = (str) => {
+//   return str
+//   .split("-")
+//   .map(
+//     (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+//     )
+//   .join('')
+// }
+
+// console.log(camelize("hello-there-matey"));
+
+
+//#2 Filter range
+
+// const filterRange = (arr, a, b) => {
+//   return arr.filter(item => (item >= a && item <= b ))
+// }
+
+// let arr = [1,2,3,4,5];
+
+// let filtered = filterRange(arr, 2, 4);
+
+// console.log(arr);
+
+// console.log(filtered);
